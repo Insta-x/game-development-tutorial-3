d328cecaaf7d384518612955ce6243ab6d977885
Insta-x <uttsadajason@outlook.com>
2024-02-25 18:28:03 +0700

    simple crouching

--- src/player/player.gd
+++ src/player/player.gd
@@ -2,11 +2,17 @@ extends CharacterBody2D
 
 
 @export_group("Movement")
-@export var move_speed := 300.0
+@export var normal_speed := 300.0
+@export var crouch_speed := 150.0
 @export var dash_speed := 600.0
 @export var jump_power := -400.0
 
 @onready var state_chart: StateChart = $StateChart
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var dash_input_checker: DashInputChecker = $DashInputChecker
+@onready var normal_collision_shape: CollisionShape2D = $NormalCollisionShape2D
+@onready var crouch_collision_shape: CollisionShape2D = $CrouchCollisionShape2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,23 +22,35 @@ var dash_direction: DashInputChecker.Direction = DashInputChecker.Direction.NONE
 
 
 #region Normal State
+func _on_normal_state_entered() -> void:
+	dash_input_checker.dash_detected.connect(_on_dash_input_checker_dash_detected)
+	normal_collision_shape.disabled = false
+
+
+func _on_normal_state_processing(delta: float) -> void:
+	sprite.flip_h = velocity.x < 0 if abs(velocity.x) > 0.1 else sprite.flip_h
+	
+	if velocity.y < 0:
+		animation_player.play("jumping")
+	elif velocity.y > 0.2:
+		animation_player.play("falling")
+	else:
+		animation_player.play("idle" if abs(velocity.x) < 0.1 else "walking")
+
+
 func _on_normal_state_physics_processing(delta: float) -> void:
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	else:
-		can_jump_count = 2
-
-	if Input.is_action_just_pressed("jump") and can_jump_count > 0:
-		velocity.y = jump_power
-		can_jump_count -= 1
-
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * move_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed)
-
+	if Input.is_action_just_pressed("crouch"):
+		state_chart.send_event("ToCrouching")
+	
+	_vertical_movement(delta)
+	_horizontal_movement(delta, normal_speed)
+	
 	move_and_slide()
+
+
+func _on_normal_state_exited() -> void:
+	dash_input_checker.dash_detected.disconnect(_on_dash_input_checker_dash_detected)
+	normal_collision_shape.disabled = true
 #endregion
 
 
@@ -40,6 +58,12 @@ func _on_normal_state_physics_processing(delta: float) -> void:
 func _on_dashing_state_entered() -> void:
 	velocity.x = dash_speed if dash_direction == DashInputChecker.Direction.RIGHT else -dash_speed
 	velocity.y = 0
+	
+	sprite.flip_h = dash_direction == DashInputChecker.Direction.LEFT
+	
+	animation_player.play("dashing")
+	
+	normal_collision_shape.disabled = false
 
 
 func _on_dashing_state_physics_processing(delta: float) -> void:
@@ -48,6 +72,61 @@ func _on_dashing_state_physics_processing(delta: float) -> void:
 
 func _on_dashing_state_exited() -> void:
 	velocity.x = 0
+	
+	normal_collision_shape.disabled = true
+#endregion
+
+
+#region Crouching State
+func _on_crouching_state_entered() -> void:
+	crouch_collision_shape.disabled = false
+
+
+func _on_crouching_state_processing(delta: float) -> void:
+	sprite.flip_h = velocity.x < 0 if abs(velocity.x) > 0.1 else sprite.flip_h
+	
+	if velocity.y < 0:
+		animation_player.play("jumping")
+	elif velocity.y > 0.2:
+		animation_player.play("falling")
+	else:
+		animation_player.play("crouching")
+
+
+func _on_crouching_state_physics_processing(delta: float) -> void:
+	if Input.is_action_just_released("crouch"):
+		state_chart.send_event("ToNormal")
+	
+	_vertical_movement(delta)
+	_horizontal_movement(delta, crouch_speed)
+	
+	move_and_slide()
+
+
+func _on_crouching_state_exited() -> void:
+	crouch_collision_shape.disabled = true
+#endregion
+
+
+#region Private Helper Functions
+## Physics process helper function. NOT TO BE ACCESSED ELSEWHERE!
+func _vertical_movement(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		can_jump_count = 2
+	
+	if Input.is_action_just_pressed("jump") and can_jump_count > 0:
+		velocity.y = jump_power
+		can_jump_count -= 1
+
+## Physics process helper function. NOT TO BE ACCESSED ELSEWHERE!
+func _horizontal_movement(delta: float, move_speed: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
 #endregion
 
 
